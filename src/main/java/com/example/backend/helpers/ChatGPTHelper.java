52458c020c3973a3cc7fe4d069368d263fc0a36c
Леонид Гарин <garin@lanit.ru>
2023-05-18 13:12:39 +0200

    Refactoring gpt helper

--- src/main/java/com/example/backend/helpers/ChatGPTHelper.java
+++ src/main/java/com/example/backend/helpers/ChatGPTHelper.java
@@ -1,18 +1,25 @@
 package com.example.backend.helpers;
 
+import static com.example.backend.helpers.utils.ChatGPTUtils.*;
+import static java.util.Spliterators.*;
+import static org.apache.commons.lang3.StringUtils.*;
+
 import com.example.backend.dto.TarotRequest;
 import com.example.backend.dto.TarotResponse;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Objects;
 import java.util.Spliterator;
-import java.util.Spliterators;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -22,8 +29,10 @@ public class ChatGPTHelper {
 
     @Value("${gpt.api.url}")
     private String url;
+    @Value("${gpt.api.auth.type}")
+    private String authType;
     @Value("${gpt.api.auth.bearer}")
-    private String auth;
+    private String bearer;
     @Value("${gpt.api.model}")
     private String model;
     @Value("${gpt.api.max_tokens}")
@@ -31,15 +40,39 @@ public class ChatGPTHelper {
     @Value("${gpt.api.temperature}")
     private String temperature;
 
+    private final HttpClient httpClient;
     private final ObjectMapper objectMapper;
 
     public TarotResponse tarotMeChatGPT(TarotRequest request) throws Exception {
-        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
+        HttpRequest httpRequest = prepareHttpRequest(request);
+        HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-        con.setRequestMethod("POST");
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("Authorization", auth);
+        JsonNode responseBody = objectMapper.readTree(httpResponse.body());
 
+        String gptResponse = StreamSupport.stream(
+                spliteratorUnknownSize(responseBody.get("choices").iterator(), Spliterator.ORDERED),
+                false)
+            .filter(Objects::nonNull)
+            .map(jsonNode -> jsonNode.get("text").asText())
+            .filter(StringUtils::isNotBlank)
+            .map(answer -> answer.replaceAll(SPECIAL_SYMBOLS_TO_EXCLUDE, EMPTY))
+            .collect(Collectors.joining("\n"));
+
+        return new TarotResponse(request.cards(), gptResponse);
+    }
+
+    private HttpRequest prepareHttpRequest(TarotRequest request) throws URISyntaxException {
+        return HttpRequest.newBuilder()
+            .POST(HttpRequest.BodyPublishers.ofByteArray(prepareBody(request)))
+            .uri(new URI(url))
+            .headers(
+                "Content-Type", "application/json",
+                "Authorization", authType + SPACE + bearer
+            )
+            .build();
+    }
+
+    private byte[] prepareBody(TarotRequest request) {
         ObjectNode objectNode = objectMapper.createObjectNode();
 
         objectNode.put("model", model);
@@ -47,27 +80,17 @@ public class ChatGPTHelper {
         objectNode.put("max_tokens", Long.parseLong(maxTokens));
         objectNode.put("temperature", Double.parseDouble(temperature));
 
-        con.setDoOutput(true);
-        con.getOutputStream().write(objectNode.toString().getBytes());
-
-        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines().reduce((a, b) -> a + b).get();
-
-        String gptResponse = StreamSupport.stream(
-                Spliterators.spliteratorUnknownSize(objectMapper.readTree(output).get("choices").iterator(), Spliterator.ORDERED), false)
-            .map(jsonNode -> jsonNode.get("text").asText()).collect(Collectors.joining("\n"));
-
-        return new TarotResponse(request.cards(), gptResponse);
+        return objectNode.toString().getBytes();
     }
 
     private String prepareQuestion(TarotRequest request) {
-        String pattern = "Hi, GPT. Tell me the TAROT without explaining each card. My question is - \"%s\". My cards are \"%s\"";
         String cards = request.cards().stream().map(card -> {
             if (card.reversed()) {
                 return card.name() + " reversed";
             }
             return card.name();
         }).collect(Collectors.joining(", "));
-        return String.format(pattern, request.text(), cards);
+        return String.format(TAROT_DEFAULT_TEMPLATE, request.text(), cards);
     }
 
 }
---
+++ src/main/java/com/example/backend/helpers/utils/ChatGPTUtils.java
@@ -0,0 +1,7 @@
+package com.example.backend.helpers.utils;
+
+public interface ChatGPTUtils {
+
+    String SPECIAL_SYMBOLS_TO_EXCLUDE = "\n";
+    String TAROT_DEFAULT_TEMPLATE = "Hi, GPT. Tell me the TAROT. My question is - \"%s\". My cards are \"%s\"";
+}
